5fe9c57458deb303ad90c2597b4e8e4152d9ecbb
Gojou-Satoru-8 <me@ankushbhowmik.com>
2024-11-29 00:41:06 +0530

    Changed title (week to semester)

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
       </head>
       <body>
         <section>
-          <h2>Goals for the week:</h2>
+          <h2>Goals for the semester:</h2>
           <h3>${userGoal.join(", ")}</h3>
         </section>
         <form action="/store-goal" method="POST">
